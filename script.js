c57e00c63fe069deb86c43bcdf153d17adc62f14
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-10 16:35:54 +0100

    Fill rows with squares

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const container= document.querySelector('#container');
 
-// const square = document.createElement('div');
-// square.classList.add('square');
-
-// container.appendChild(square);
-
-let number = 16;
+let number = 15;
 
 for (let i=0;i<number;i++){
     const row=document.createElement('div');
     row.classList.add('row');
     container.appendChild(row);
-}
-//     for (let j=0;j<number;j++){
 
-//     }
-// }
+    for (let j=0;j<number;j++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+    }
+}
 
